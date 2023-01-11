754f3bbdecfa14804ddc63330e77811d9bac7a39
marilu777 <maria.valentes@hotmail.com>
2023-01-11 14:55:21 +0000

    test

--- src/com/mycompany/bankingapp/BankingApp.java
+++ src/com/mycompany/bankingapp/BankingApp.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.bankingapp;
 
-/**
- *
- * @author Maria
- */
 public class BankingApp {
 
     public static void main(String[] args) {
